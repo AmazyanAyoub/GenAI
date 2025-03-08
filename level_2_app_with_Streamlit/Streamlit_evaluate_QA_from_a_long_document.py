3f43b5a7974303c00a104127d83e6e9b22ffc6da
ayoubamazian <ayoubamazyan26@gmail.com>
2025-03-08 18:37:50 +0100

    finish the level 2 apps

--- level_2_app_with_Streamlit/Streamlit_evaluate_QA_from_a_long_document.py
+++ level_2_app_with_Streamlit/Streamlit_evaluate_QA_from_a_long_document.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from langchain import PromptTemplate
 from langchain_groq import ChatGroq
-from langchain.schema import HumanMessage
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.evaluation.qa import QAEvalChain
 
-st.set_page_config(page_title="Evaluate QA from a long document")
-st.header("Evaluate QA from a long document")
+st.set_page_config(
+    page_title="Evaluate a RAG App"
+)
+st.title("Evaluate a RAG App")
 
 def generate_response(uploaded_file, Groq_api_key, query_text, response_text):
 
@@ -65,3 +65,66 @@ def generate_response(uploaded_file, Groq_api_key, query_text, response_text):
 
     return response
 
+with st.expander("Evaluate the quality of a RAG APP"):
+    st.write("""
+        To evaluate the quality of a RAG app, we will
+        ask it questions for which we already know the
+        real answers.
+        
+        That way we can see if the app is producing
+        the right answers or if it is hallucinating.
+    """)
+
+uploaded_file = st.file_uploader(
+    "Upload a .txt document",
+    type="txt"
+)
+
+query_text = st.text_input(
+    "Enter a question you have already fact-checked:",
+    placeholder="Write your question here",
+    disabled=not uploaded_file
+)
+
+response_text = st.text_input(
+    "Enter the real answer to the question:",
+    placeholder="Write the confirmed answer here",
+    disabled=not uploaded_file
+)
+
+result = []
+with st.form(
+    "myform",
+    clear_on_submit=True
+):
+    Groq_api_key = st.text_input(
+        "Groq api key:",
+        type="password",
+        disabled=not (uploaded_file and query_text)
+    )
+    submitted = st.form_submit_button(
+        "Submit",
+        disabled=not (uploaded_file and query_text)
+    )
+    if submitted and Groq_api_key.startswith("sk-"):
+        with st.spinner(
+            "Wait, please. I am working on it..."
+            ):
+            response = generate_response(
+                uploaded_file,
+                Groq_api_key,
+                query_text,
+                response_text
+            )
+            result.append(response)
+            del Groq_api_key
+            
+if len(result):
+    st.write("Question")
+    st.info(response["predictions"][0]["question"])
+    st.write("Real answer")
+    st.info(response["predictions"][0]["answer"])
+    st.write("Answer provided by the AI App")
+    st.info(response["predictions"][0]["result"])
+    st.write("Therefore, the AI App answer was")
+    st.info(response["graded_outputs"][0]["results"])
